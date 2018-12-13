ad4340af8c86bb80664f3af8f4d310489afcd1f4
codejoncode <jonathanjholloway@gmail.com>
2018-12-13 12:49:52 -0600

    Updates

---
+++ precache-manifest.6d784d1aebeb42dc510104a691fbe597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "898ac658411cdb17efd4",
+    "url": "/Conways-Life/static/js/main.898ac658.chunk.js"
+  },
+  {
+    "revision": "9b02c182b33c30d8e33d",
+    "url": "/Conways-Life/static/js/1.9b02c182.chunk.js"
+  },
+  {
+    "revision": "898ac658411cdb17efd4",
+    "url": "/Conways-Life/static/css/main.93b5cb83.chunk.css"
+  },
+  {
+    "revision": "fb2a905c4fb08d0f72d266a58e98b08e",
+    "url": "/Conways-Life/index.html"
+  }
+];
